e55548dd8b08fcbdb221168a544fb0860f182c44
Matthew Soh <matthewsoh@berkeley.edu>
2019-04-08 10:49:37 -0700

    Add hello world command

--- theia-collaborative-frontend-plugin/src/theia-collaborative-frontend-plugin-frontend.ts
+++ theia-collaborative-frontend-plugin/src/theia-collaborative-frontend-plugin-frontend.ts
@@ -7,6 +7,13 @@ import * as theia from '@theia/plugin';
 
 export function start(context: theia.PluginContext) {
     console.log("Theia Collaborative Frontend Plugin loaded");
+    const informationMessageTestCommand = {
+        id: 'hello-world-example-generated',
+        label: "Hello World"
+    };
+    theia.commands.registerCommand(informationMessageTestCommand, (...args: any[]) => {
+        theia.window.showInformationMessage('Hello World!');
+    });
 }
 
 export function stop() {
